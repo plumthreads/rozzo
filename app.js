402abb4c2baf5a45850a0b0a516e7dc22c6bc179
Dylan-Tran <43786689+Dylan-Tran@users.noreply.github.com>
2019-09-21 17:14:15 -0400

    Enter

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+
+app.set('views', path.join(__dirname, 'views'));
+
 //Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,11 +16,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 //routes folder
-var get = require('./routes/get');
+const get = require('./routes/get');
 app.use(get);
 
+/*
 var post = require('./routes/post');
 app.use(post);
+*/
 
 app.listen(5500, ()=>{
     console.log("Server is on port 57");
